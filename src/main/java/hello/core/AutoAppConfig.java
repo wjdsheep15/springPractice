ad6f4cd16d5a71f804cf88c5801a295b25061f23
wjdsheep15 <wjd15sheep@gmail.com>
2025-04-24 16:42:35 +0900

    Feat: allBean

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,16 +1,12 @@
 package hello.core;
 
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( //자동으로 스프링빈을 꺼내 자동으로 스프링 빈에 등록
-        basePackages = "hello.core.member", // 탐색할 패키지 root 지정 why? 지정하지 않으면 라이브러리까지 다 확인하기 때문에 시간이 많이 듬 그렇기 때문에 지정
-        basePackageClasses = AutoAppConfig.class, // 현재 파일의 패키지를 기본으로 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // AppConfig 설정을 제외하는 필터
 )
 public class AutoAppConfig {
--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -13,7 +13,7 @@ public class AutoAppConfigTest {
 
     @Test
     void basicScan() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
